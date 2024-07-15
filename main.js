4e3b48c563c0accf6e124f61979038028a485ba8
kien192 <trungkien19203@gmail.com>
2024-07-15 23:07:53 +0700

    upload database product

--- main.js
+++ main.js
@@ -10,14 +10,6 @@ server.use(middlewares)
 server.get('/echo', (req, res) => {
     res.jsonp(req.query)
 })
-
-
-
-
-
-
-
-
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
